626f386e63875216c82ab4c36aa7a4a83fc05149
Harshil Sharma <harshilsharma.dev@gmail.com>
2023-12-29 21:02:50 +0530

    .

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,1 +1,6 @@
-console.log("meow");
+let number = prompt("Enter a number: ");
+if (number % 5 === 0) {
+    console.log(number, "is multiple of 5.")
+} else {
+    console.log(number, "is not a multiple of 5.")
+}
