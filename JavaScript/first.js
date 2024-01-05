e428aeec15c628c7af8f1798bbc22e1a03fe1f08
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-05 23:36:57 +0530

    lessons learnt on 5 jan 2024

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,4 +1,20 @@
-let arr = [1, 2, 3, 4, 5];
-arr.forEach((val) => {
-    console.log(val * val * val);
+let n = prompt("Enter a number: ");
+
+let arr = [];
+for (let i = 1; i <= n; i++) {
+    arr[i-1] = i;
+}
+
+console.log(arr);
+
+let sum = arr.reduce((prev, curr) => {
+    return prev + curr;
 })
+
+console.log(sum);
+
+let product = arr.reduce((prev, curr) => {
+    return prev * curr;
+})
+
+console.log(product);
