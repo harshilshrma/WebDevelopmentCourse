45aaf3a7b8e7e6bf530554c15dbf654896d1bfaa
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-09 21:41:35 +0530

    lessons learnt on 09 january 2024

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,3 +1,9 @@
-let div = document.querySelector("div");
+// let h2 = document.querySelector("h2");
+// console.dir(h2.innerText += " from Apna College");
 
-let heading = document.querySelector("h2");
+let divs = document.querySelectorAll(".box");
+console.dir(divs);
+
+for (div of divs) {
+    console.log(div.innerText);
+}
