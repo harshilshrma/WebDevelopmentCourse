cdeaedcf23a1ef4f668b453344cc8ee2c2c44b29
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-09 21:13:28 +0530

    .

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,20 +1,3 @@
-let n = prompt("Enter a number: ");
+let div = document.querySelector("div");
 
-let arr = [];
-for (let i = 1; i <= n; i++) {
-    arr[i-1] = i;
-}
-
-console.log(arr);
-
-let sum = arr.reduce((prev, curr) => {
-    return prev + curr;
-})
-
-console.log(sum);
-
-let product = arr.reduce((prev, curr) => {
-    return prev * curr;
-})
-
-console.log(product);
+let heading = document.querySelector("h2");
