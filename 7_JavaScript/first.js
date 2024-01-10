e7bdf403920604a7ac15d5786b30085dbea73630
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-10 21:11:01 +0530

    created a dark/light mode button

--- 7_JavaScript/first.js
+++ 7_JavaScript/first.js
@@ -1,7 +1,15 @@
-let btn = document.querySelector("#btn");
+let modebtn = document.querySelector("#mode");
+let currMode = "light";
 
-btn.addEventListener("click", () => {
-    console.log("scdvs");
-});
-
-
+modebtn.addEventListener("click", () => {
+    if (currMode == "light") {
+        currMode = "dark";
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+    } else {
+        currMode = "light";
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+    }
+    console.log(currMode);
+});  
