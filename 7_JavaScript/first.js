a968c4fa27eb40cadc5a6676b64369040086be9a
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-17 00:19:02 +0530

    started learning sync in JS

--- 7_JavaScript/first.js
+++ 7_JavaScript/first.js
@@ -1,12 +1,3 @@
-let a = 10;
-let b = 12;
-
-console.log("a + b =", a + b);
-console.log("a + b =", a + b);
-try {
-    console.log("a + b =", a + c);
-} catch(err) {
-    console.log("ERROR: ", err);
-}
-console.log("a + b =", a + b);
-console.log("a + b =", a + b);
+setTimeout(() => {
+    console.log("helo");
+}, 1000)
