0bb34fb0ec45aec1b6cac35409d427a02041fd7b
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-16 22:41:09 +0530

    did practice ques on classes and objects

--- 7_JavaScript/first.js
+++ 7_JavaScript/first.js
@@ -1,19 +1,29 @@
-class Parent {
-    eat() {
-        console.log("i am eating");
+let DATA = "secret";
+
+class User {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
     }
 
-    sleep() {
-        console.log("selees");
+    viewData() {
+        console.log("Name: ", DATA);
+    }
+
+}
+
+class Admin extends User {
+    constructor(name, email) {
+        super(name, email);
+
+    }
+
+    editData() {
+        DATA = "NEW VALUE";
     }
 }
 
-class Engineer extends Parent {
-    work() {
-        console.log("");
-    }
-}
+let student1 = new User("harshil", "abc@email.com");
+let student2 = new User("fgbc", "abc@dsfv.com");
 
-let harshilObj = new Engineer;
-
-
+let admin1 = new Admin("admin, ejbe@kfnasdk.com")
