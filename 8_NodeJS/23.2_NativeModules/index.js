b70cfa76f46d4f6567a3843e6ed098f32997e827
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-25 22:09:11 +0530

    learnt file system in nodeJS

--- 8_NodeJS/23.2_NativeModules/index.js
+++ 8_NodeJS/23.2_NativeModules/index.js
@@ -1,6 +1,15 @@
+const { error } = require("console");
 const fs = require("fs");
 
-fs.writeFile("message.txt")
+fs.writeFile("message.txt",  "HELLOO MEOWWW", (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+});
+
+fs.readFile("./message.txt", "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
 
 
 
